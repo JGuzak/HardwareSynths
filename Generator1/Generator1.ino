1b2ad1b6932b40910ef8c43337c1ee8d888c44f6
Jordan Guzak <jguzak@live.com>
2017-05-13 20:18:00 -0400

    Added wave types

---
+++ Generator1/Generator1.ino
@@ -0,0 +1,170 @@
+/*  Jordan Guzak
+ *  Generator 1
+ * 
+ */
+
+#include <MozziGuts.h>
+#include <Oscil.h>
+#include <tables/sin2048_int8.h>
+#include <tables/triangle2048_int8.h>
+#include <tables/square_no_alias_2048_int8.h>
+#include <tables/saw2048_int8.h>
+#include <LowPassFilter.h>
+#include <DCfilter.h>
+
+const bool SERIAL_DEBUG = true;
+const char volume_pin = 0;
+const char pitch_pin = 1;
+
+const char wave_type_pin = 0;
+const int WAVE_TYPES = 4;
+
+const int lpf_frequency_pin = 2;
+const int lpf_resonance_pin = 3;
+
+byte g_volume = 1;
+int g_pitch = 440;
+int g_wave_type = 0, g_next_wave_type = 0;
+int g_lpf_frequency = 0, g_lpf_resonance = 10;
+
+bool buttonClicked = false;
+
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> sin1(SIN2048_DATA);
+Oscil <TRIANGLE2048_NUM_CELLS, AUDIO_RATE> tri1(TRIANGLE2048_DATA);
+Oscil <SQUARE_NO_ALIAS_2048_NUM_CELLS, AUDIO_RATE> sqr1(SQUARE_NO_ALIAS_DATA);
+Oscil <SAW2048_NUM_CELLS, AUDIO_RATE> saw1(SAW2048_DATA);
+
+Oscil* curOsc;
+
+LowPassFilter lpf;
+
+#define CONTROL_RATE 64
+
+void setup() {
+    pinMode(wave_type_pin, INPUT);
+
+    curOsc = sin1;
+    
+    curOsc.setFreq(g_pitch);
+    lpf.setCutoffFreq(g_lpf_frequency);
+    lpf.setResonance(g_lpf_resonance);
+    
+    if (SERIAL_DEBUG) {
+        Serial.begin(115200);
+    }
+    
+    startMozzi(CONTROL_RATE);
+}
+
+void updateControl() {
+    volatile int temp;
+
+    // update volume
+    temp = map(mozziAnalogRead(volume_pin), 0, 1023, 0, 255);
+    if (g_volume != temp) {
+        g_volume = temp;
+
+        if (SERIAL_DEBUG) {
+            Serial.print("Volume: ");
+            Serial.print(g_volume);
+            Serial.print("\n");
+        }  
+    }
+    
+    // update pitch
+    temp = map(mozziAnalogRead(pitch_pin), 0, 1023, 20, 700);
+    if (g_pitch != temp) {
+        g_pitch = temp;
+        osc1.setFreq(g_pitch);
+
+        if (SERIAL_DEBUG) {
+            Serial.print("Pitch: ");
+            Serial.print(g_pitch);
+            Serial.print("\n");
+        }
+    }
+
+    // wave type selection
+    if (g_wave_type != g_next_wave_type) {
+        switch(g_next_wave_type) {
+          case 0:
+              curOsc = sin1;
+              break;
+          case 1:
+              curOsc = tri1;
+              break;
+          case 2:
+              curOsc = sqr1;
+              break;
+          case 3:
+              curOsc = saw1;
+              break;
+          default:
+              break;
+        }
+
+        if (SERIAL_DEBUG) {
+            Serial.println("Waveform changed");
+            Serial.println();
+        }
+        
+        g_wave_type = g_next_wave_type;
+    }
+
+    // update lpf frequency
+    temp = map(mozziAnalogRead(lpf_frequency_pin), 0, 1023, 0, 255);
+    if (g_lpf_frequency != temp) {
+        g_lpf_frequency = temp;
+        lpf.setCutoffFreq(g_lpf_frequency);
+
+        if (SERIAL_DEBUG) {
+            Serial.print("LPF Frequency: ");
+            Serial.print(g_lpf_frequency);
+            Serial.print("\n");
+        }
+    }
+
+    // update lpf resonance
+    /*
+    temp = map(mozziAnalogRead(lpf_resonance_pin), 0, 1023, 0, 255);
+    if (g_lpf_resonance != temp) {
+        g_lpf_resonance = temp;
+        lpf.setResonance(g_lpf_resonance);
+
+        if (SERIAL_DEBUG) {
+            Serial.print("LPF Resonance: ");
+            Serial.print(g_lpf_resonance);
+            Serial.print("\n");
+        }
+    }
+    */
+    
+}
+
+int updateAudio() {
+    //char sig = lpf.next(curOsc.next() * g_volume);
+    //return  (int) sig >> 8;
+    return (lpf.next(curOsc.next()) * g_volume) >> 8;
+}
+
+void loop() {
+
+    if (digitalRead(wave_type_pin) == HIGH && !buttonClicked) {
+        if (SERIAL_DEBUG) {
+            Serial.println("Click");
+        }
+
+        g_next_wave_type = (g_next_wave_type + 1) % (WAVE_TYPES);
+        
+        buttonClicked = true;
+    }
+
+    if (digitalRead(wave_type_pin) == LOW && buttonClicked) {
+        if (SERIAL_DEBUG) {
+            Serial.println("Clack");
+        }
+        buttonClicked = false;
+    }
+  
+    audioHook();
+}
